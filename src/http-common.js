5c6282e674c71f7bd037e9298324246b85ba29ef
Rafal Monka <monka.rafal@gmail.com>
2020-04-27 16:02:51 +0200

    process.env

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 //require('dotenv').config();
 
-console.log("process.env="+JSON.stringify(process.env));
+console.log(new Date()+" process.env="+JSON.stringify(process.env));
+
+var url = process.env.NODE_ENV === "development" ? "http://localhost:8083" : "http://react-buylist-git-memory.apps.us-east-1.starter.openshift-online.com";
 
 export default axios.create({
-  baseURL: "http://"+process.env.REACT_APP_BASE_API_URL+":"+process.env.REACT_APP_BASE_API_PORT+"/api",//"http://localhost:8083/api",
+  //baseURL: "http://"+process.env.REACT_APP_BASE_API_URL+":"+process.env.REACT_APP_BASE_API_PORT+"/api",//"http://localhost:8083/api",
+  baseURL: url + "/api",
   headers: {
     "Content-type": "application/json"
   }
