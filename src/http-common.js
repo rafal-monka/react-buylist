a2d8ad219894dc55e08ee919fec0ed0ff58e98cd
Rafal Monka <monka.rafal@gmail.com>
2020-04-27 12:19:31 +0200

    env

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+//require('dotenv').config();
+
+console.log("process.env="+JSON.stringify(process.env));
 
 export default axios.create({
-  baseURL: "http://localhost:8083/api",
+  baseURL: "http://"+process.env.REACT_APP_BASE_API_URL+":"+process.env.REACT_APP_BASE_API_PORT+"/api",//"http://localhost:8083/api",
   headers: {
     "Content-type": "application/json"
   }
