83c1eddafb7e2aa6419ee15d5268281f4d748a0b
Rafal Monka <monka.rafal@gmail.com>
2020-10-07 16:17:53 +0200

    package.json heroku "start": "serve -s build"

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h4>Do the shopping being prepared</h4>
+            <h4>Be prepared, do the shopping.</h4>
             <pre>{JSON.stringify(process.env, null, 3)}</pre>
         </div>
     )
